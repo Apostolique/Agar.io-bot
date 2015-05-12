6e43359d2025a43009e70319532ea2105961222b
Apostolique <vynecompiler@gmail.com>
2015-05-11 20:53:37 -0400

    TYPO

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@
 // @namespace   Vawlt
 // @include     http://agar.io/*
 // @version     1
-// @@updateURL  https://raw.githubusercontent.com/Apostolique/Agar.io-bot/master/bot.js
+// @updateURL   https://raw.githubusercontent.com/Apostolique/Agar.io-bot/master/bot.js
 // @grant       none
 // @author      twitch.tv/apostolique
 // @run-at document-start
