f38b056cba9a0a3f8fd52e910cff180ae81f4f2c
Simas Skilinskas <simskil@gmail.com>
2015-06-18 14:31:29 +0300

    Personalize bot name for myself

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "simaks",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
