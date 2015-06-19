30697f84e4d651f3a28c65efc6a958c250d73cf5
zaho52 <zahidsoysal@hotmail.com>
2015-06-19 18:32:21 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/
+// @include     http://http://agar.turkish-gaming.com/
 // @version     3.05
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
