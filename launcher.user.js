08faa4451a08e0c2f75e13df035d0c6fbe1f618c
ikclethanhdat <ikclethanhdat1@gmail.com>
2015-06-22 08:46:13 +0700

    Update launcher.user.js
    Changed name

--- launcher.user.js
+++ launcher.user.js
@@ -889,7 +889,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  names = ["NotReallyABot"],
+  names = ["GOD"],
   originalName = names[Math.floor(Math.random() * names.length)],
   sessionScore = 0,
   serverIP = "",
