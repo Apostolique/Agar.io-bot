089712dbb1386238f134b31637aec55597fbd25a
batuhan13tr <batuhan_06_21@hotmail.com>
2015-07-12 20:14:25 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1218,7 +1218,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["Turkey <3"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
