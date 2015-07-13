77028ac06487e3f7ee14af7926cde16c8cd4153a
Fossilium <stefan,super@gmx.net>
2015-07-13 19:29:22 +0200

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1221,7 +1221,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["NASA"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
