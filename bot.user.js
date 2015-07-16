e78c4a573457993c9d2f292bece3e6c3c2f3bbb8
fasfasfasfasfasdasd <berkaygyr01@hotmail.com>
2015-07-16 15:25:50 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://illeg4lagar.com/*
 // @version     3.55
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
