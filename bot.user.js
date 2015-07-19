66cbd8ea20fb6118fea88491f9a0d1c29056bb97
NPWR <npwrmusic@gmail.com>
2015-07-19 20:16:09 +0200

    Typo

--- bot.user.js
+++ bot.user.js
@@ -970,7 +970,6 @@ console.log("Running Apos Bot!");
                         //tempMoveX = line1[0];
                         //tempMoveY = line1[1];
                     } else if (badAngles.length > 0 && goodAngles == 0) {
-                        //TODO: CODE TO HANDLE WHEN THERE IS NO GOOD ANGLE BUT THERE ARE ENEMIES AROUND!!!!!!!!!!!!!
 						var angleWeights = [] //Put weights on the angles according to enemy distance
 						for (var i = 0; i < allPossibleThreats.length; i++){
 							var dist = computeDistance(player[k].x,player[k].y,allPossibleThreats[i].x,allPossibleThreats[i].y);
@@ -986,6 +985,7 @@ console.log("Running Apos Bot!");
 							}
 						}
 						var line = followAngle(finalAngle,player[k].x,player[k].y,f.verticalDistance());
+						destinationChoices.push(line);
                     } else if (clusterAllFood.length > 0) {
                         for (var i = 0; i < clusterAllFood.length; i++) {
                             //console.log("mefore: " + clusterAllFood[i][2]);
