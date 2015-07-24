e6a1b740f5cdd4ca90cd158c61ed237317a15338
Timtech <timothy@timtechsoftware.com>
2015-07-24 00:07:49 -0400

    Improved code readability

--- launcher.user.js
+++ launcher.user.js
@@ -153,12 +153,12 @@ console.log("Running Bot Launcher!");
             fa = a.clientX;
             ga = a.clientY;
             Aa();
-            V()
+            V();
         };
         G.onmousemove = function(a) {
             fa = a.clientX;
             ga = a.clientY;
-            Aa()
+            Aa();
         };
         G.onmouseup = function() {};
         /firefox/i.test(navigator.userAgent) ? document.addEventListener("DOMMouseScroll", Ra, !1) : document.body.onmousewheel = Ra;
@@ -171,7 +171,7 @@ console.log("Running Bot Launcher!");
                 32 != l.keyCode || a || (V(), H(17), a = !0);
                 81 != l.keyCode || b || (H(18), b = !0);
                 87 != l.keyCode || c || (V(), H(21), c = !0);
-                27 == l.keyCode && Sa(!0)
+                27 == l.keyCode && Sa(!0);
 
                 //UPDATE
                 keyAction(l);
@@ -180,7 +180,7 @@ console.log("Running Bot Launcher!");
         d.onkeyup = function(l) {
             32 == l.keyCode && (a = !1);
             87 == l.keyCode && (c = !1);
-            81 == l.keyCode && b && (H(19), b = !1)
+            81 == l.keyCode && b && (H(19), b = !1);
         };
         d.onblur = function() {
             H(19);
@@ -214,10 +214,10 @@ console.log("Running Bot Launcher!");
                 !g.N() || g.R || 20 >= g.size * h || (d = Math.max(g.size, d), a = Math.min(g.x, a), b = Math.min(g.y, b), c = Math.max(g.x, c), l = Math.max(g.y, l))
             }
             X = rb.ka({
-                ca: a - (d + 100),
-                da: b - (d + 100),
-                oa: c + (d + 100),
-                pa: l + (d + 100),
+                ca: a - d + 100,
+                da: b - d + 100,
+                oa: c + d + 100,
+                pa: l + d + 100,
                 ma: 2,
                 na: 4
             });
@@ -599,7 +599,7 @@ console.log("Running Bot Launcher!");
     }
 
     function screenToGameY(y) {
-        return (y - getHeight() / 2) / getRatio() + getY();;
+        return (y - getHeight() / 2) / getRatio() + getY();
     }
 
     window.drawPoint = function(x_1, y_1, drawColor, text) {
@@ -706,7 +706,7 @@ console.log("Running Bot Launcher!");
 
     function hb() {
         var a;
-        a = 1 * Math.max(r / 1080, m / 1920);
+        a = Math.max(r / 1080, m / 1920);
         return a *= J
     }
 
@@ -732,13 +732,12 @@ console.log("Running Bot Launcher!");
         C = b;
         if (0 < k.length) {
             yb();
-            for (var c =
-                    a = 0, d = 0; d < k.length; d++) k[d].P(), a += k[d].x / k.length, c += k[d].y / k.length;
+            for (var c = a = 0, d = 0; d < k.length; d++) k[d].P(), a += k[d].x / k.length, c += k[d].y / k.length;
             aa = a;
             ba = c;
             ca = h;
             s = (s + a) / 2;
-            t = (t + c) / 2
+            t = (t + c) / 2;
         } else s = (29 * s + aa) / 30, t = (29 * t + ba) / 30, h = (9 * h + ca * hb()) / 10;
         qb();
         Aa();
@@ -787,8 +786,7 @@ console.log("Running Bot Launcher!");
         }
         customRender(f);
         if (Ga) {
-            na = (3 *
-                na + Ea) / 4;
+            na = (3 * na + Ea) / 4;
             oa = (3 * oa + Fa) / 4;
             f.save();
             f.strokeStyle = "#FFAAAA";
@@ -799,7 +797,7 @@ console.log("Running Bot Launcher!");
             f.beginPath();
             for (d = 0; d < k.length; d++) f.moveTo(k[d].x, k[d].y), f.lineTo(na, oa);
             f.stroke();
-            f.restore()
+            f.restore();
         }
         f.restore();
         z && z.width && f.drawImage(z, m - z.width - 10, 10);
@@ -811,7 +809,7 @@ console.log("Running Bot Launcher!");
 
         var nbSeconds = 0;
         if (getPlayer().length > 0) {
-            //nbSeconds = (currentDate.getSeconds() + (currentDate.getMinutes() * 60) + (currentDate.getHours() * 60 * 60)) - (lifeTimer.getSeconds() + (lifeTimer.getMinutes() * 60) + (lifeTimer.getHours() * 60 * 60));
+            //nbSeconds = currentDate.getSeconds() + currentDate.getMinutes() * 60 + currentDate.getHours() * 3600 - lifeTimer.getSeconds() - lifeTimer.getMinutes() * 60 - lifeTimer.getHours() * 3600;
             nbSeconds = (currentDate.getTime() - lifeTimer.getTime())/1000;
         }
 
@@ -1046,9 +1044,8 @@ console.log("Running Bot Launcher!");
         f.save();
         f.strokeStyle = ta ? "#AAAAAA" : "#000000";
         f.globalAlpha = .2 * h;
-        for (var a = m / h, b = r / h, c = (-s + a / 2) % 50; c < a; c += 50) f.beginPath(), f.moveTo(c * h - .5, 0), f.lineTo(c * h - .5, b * h), f.stroke();
-        for (c = (-t + b / 2) % 50; c < b; c +=
-            50) f.beginPath(), f.moveTo(0, c * h - .5), f.lineTo(a * h, c * h - .5), f.stroke();
+        for (var a = m / h, b = r / h, c = (a / 2 - s) % 50; c < a; c += 50) f.beginPath(), f.moveTo(c * h - .5, 0), f.lineTo(c * h - .5, b * h), f.stroke();
+        for (c = (b / 2 - t) % 50; c < b; c += 50) f.beginPath(), f.moveTo(0, c * h - .5), f.lineTo(a * h, c * h - .5), f.stroke();
         f.restore()
     }
 
