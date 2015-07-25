1e967661c0506244d18fc290b26d66d9eaceed8b
dorporati <dorile1234@gmail.com>
2015-07-25 11:32:51 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1294,7 +1294,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["DorPorati"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
