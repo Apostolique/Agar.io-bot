9fd88589da6f41645b3749074d836008f531c5c3
Apostolique <vynecompiler@gmail.com>
2015-08-03 17:13:43 -0400

    New version number

--- bot.user.js
+++ bot.user.js
@@ -2,12 +2,12 @@
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.551
+// @version     3.56
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.551;
+var aposBotVersion = 3.56;
 
 //TODO: Team mode
 //      Detect when people are merging
