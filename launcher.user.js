8287a632ce0ce95e83aae1f0639e655544c07b1f
EmirHan7 <emirhanvj@gmail.com>
2015-08-24 02:50:10 +0300

    benim launcher

--- launcher.user.js
+++ launcher.user.js
@@ -1290,7 +1290,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["Emirhan"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
