f5ec4ac6552871290981fb49b7b997e8ff433da8
Apostolique <vynecompiler@gmail.com>
2015-08-30 22:49:31 -0400

    Value tuning

--- bot.user.js
+++ bot.user.js
@@ -24,12 +24,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.62
+// @version     3.63
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.62;
+var aposBotVersion = 3.63;
 
 //TODO: Team mode
 //      Detect when people are merging
