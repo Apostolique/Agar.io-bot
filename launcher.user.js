2f8dc143ee0b216e70aed2a93b3c1eeda4723e2f
Ermiya Eskandary <eskandaryermiya@gmail.com>
2016-04-10 11:07:58 +0100

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,5 +1,5 @@
 /*The MIT License (MIT)
-Copyright (c) 2015 Apostolique
+Copyright (c) 2016 Ermiya Eskandary
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -16,12 +16,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 // ==UserScript==
-// @name        AposLauncher
-// @namespace   AposLauncher
+// @name        AgarLauncher
+// @namespace   AgarLauncher
 // @include     http://agar.io/*
 // @version     4.149
 // @grant       none
-// @author      http://www.twitch.tv/apostolique
+// @author      Ermiya Eskandary
 // ==/UserScript==
 var aposLauncherVersion = 4.149;
 
